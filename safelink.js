90e72a0c4ec9f19d0c57822d681daaa80e8620a8
Abdul karim <47812298+zonagratisoft@users.noreply.github.com>
2019-03-07 12:29:58 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -30,6 +30,7 @@ if(x=="6"){xxx="windows-android-403.html"}
 if(x=="7"){xxx="instagram-6180.html"}
 if(x=="8"){xxx="shadow-of-colossus-ps4-review.html"}
 if(x=="9"){xxx="what-to-look-for-in-car-audio.html"}
+ if(x=="10"){xxx="can-sms-marketing-get-profits-to.html"}
  a_to_ve[i].href="https://zonagratissoft.blogspot.com/2019/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
